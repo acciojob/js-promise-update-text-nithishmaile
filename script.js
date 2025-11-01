523dd80535f2b52106481aceffad84b92dbd1f4d
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-11-01 08:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+//your JS code here. If required.\
+const output=document.getElementById("output");
+
+const promise=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		output.innerText="Hello, world!"
+	},1000)
+})
